633efca711a68c9e688f5da1c9520e29d8abb059
Andrew MacDonald <andrew@maccas.net>
2016-05-08 17:05:36 +1000

    Update versioneer

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@ import os
 from io import open
 
 import versioneer
-versioneer.versionfile_source = 'catchment_tools/_version.py'
-versioneer.versionfile_build = 'catchment_tools/_version.py'
-versioneer.tag_prefix = 'v'
-versioneer.parentdir_prefix = 'catchment_tools-'
 
 from setuptools import setup
 try:
